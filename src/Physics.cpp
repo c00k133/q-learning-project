c3532a9454c7e6375ddb5423512645ad5546b359
Anton <anton.eklund@aalto.fi>
2018-11-27 20:46:15 +0200

    Took away some unneccesary stuff and started changing so that the user can decide how many joints. NOTE: This is not yet made to work with rest of the project or even by itself!

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -4,66 +4,71 @@
 #include <iostream>
 
 
-int main (int argc, char** argv) {
+int main(int argc, char** argv) {
   B2_NOT_USED(argc);
   B2_NOT_USED(argv);
-  
+
   // Define the gravity vector.
   b2Vec2 gravity(0.0f, -9.81f);
-  
+
   // Construct a world object, which will hold and simulate the rigid bodies.
   b2World world(gravity);
-  
+
   // Define the ground body.
   b2BodyDef ground_body_def;
   ground_body_def.position.Set(0.0f, -10.0f);
-  
+
   // Call the body factory which allocates memory for the ground body
   // from a pool and creates the ground box shape (also from a pool).
   // The body is also added to the world.
   b2Body* ground_body = world.CreateBody(&ground_body_def);
-  
+
   // Define the ground box shape.
   b2PolygonShape ground_box;
-  
+
   // The extents are the half-widths of the box.
   ground_box.SetAsBox(50.0f, 10.0f);
-  
+
   // Add the ground fixture to the ground body.
   ground_body->CreateFixture(&ground_box, 0.0f);
-  
+
+  // The amount of joints the user wants the worm to have.
+  unsigned int joints_wanted = 2;
+
+  // prev_body is a variable that tells where the new joint should connect.
   b2Body* prev_body = NULL;
 
   // Define the first bone.
-  
-    b2PolygonShape first_bone;
-    first_bone.SetAsBox(0.5f, 2.0f);
 
-    b2BodyDef first_body_def;
-    first_body_def.type = b2_dynamicBody;
-    first_body_def.position.Set(0.0f, 13.0f);
-    b2Body* first_body = world.CreateBody(&first_body_def);
+  b2PolygonShape first_bone;
+  first_bone.SetAsBox(0.5f, 2.0f);
 
-    b2FixtureDef first_fixture_def;
-    first_fixture_def.shape = &first_bone;
-    first_fixture_def.density = 1.0f;
-    first_body->CreateFixture(&first_fixture_def);
+  b2BodyDef first_body_def;
+  first_body_def.type = b2_dynamicBody;
+  first_body_def.position.Set(0.0f, 13.0f);
+  b2Body* first_body = world.CreateBody(&first_body_def);
 
-    // Declaring the previous body for linking the next body to it.
-    prev_body = first_body;
-  
+  b2FixtureDef first_fixture_def;
+  first_fixture_def.shape = &first_bone;
+  first_fixture_def.density = 1.0f;
+  first_body->CreateFixture(&first_fixture_def);
+
+  // Declaring the previous body for linking the next body to it.
+  prev_body = first_body;
+
+  b2Body* new_body = NULL;
 
   // Define another bone and connect it to the previous one with a joint.
-  
+  for (unsigned int amount_of_joints = 0; amount_of_joints < joints_wanted; amount_of_joints++) {
     b2PolygonShape new_bone;
     new_bone.SetAsBox(0.5f, 2.0f);
-  
+
     // Define the dynamic body. We set its position and call the body factory.
     b2BodyDef new_body_def;
     new_body_def.type = b2_dynamicBody;
     new_body_def.position.Set(0.0f, 7.0f);
-    b2Body* new_body = world.CreateBody(&new_body_def);
-  
+    new_body = world.CreateBody(&new_body_def);
+
     /*
     // Define another box shape for our dynamic body.
     b2EdgeShape bone;
@@ -89,38 +94,33 @@ int main (int argc, char** argv) {
     b2RevoluteJoint* joint = (b2RevoluteJoint*)world.CreateJoint(&joint_def);
 
     prev_body = new_body;
-    
+  }
+
   // Prepare for simulation. Typically we use a time step of 1/60 of a
   // second (60Hz) and 10 iterations. This provides a high quality simulation
   // in most game scenarios.
   float32 time_step = 1.0f / 60.0f;
   int32 velocity_iterations = 6;
   int32 position_iterations = 2;
-  
+
+
+
   // This is our little game loop.
   for (int32 i = 0; i < 300; ++i) {
     // Instruct the world to perform a single step of simulation.
     // It is generally best to keep the time step and iterations fixed.
     world.Step(time_step, velocity_iterations, position_iterations);
-    
+
     // Now print the position and angle of the body.
     b2Vec2 position = new_body->GetPosition();
     float32 angle = new_body->GetAngle();
     b2Vec2 speed = new_body->GetLinearVelocity();
-  
+
     printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
   }
-  
+
   // When the world destructor is called, all bodies and joints are freed. This can
   // create orphaned pointers, so be careful about your world management.
   std::cin.get();
   return 0;
 }
-
-auto create_bot() {
-  
-
-
-
-}
-
