1df61ff2e742b621629211234293844d46437c7e
Neergaard Axel <neergaa1@albatrossi.org.aalto.fi>
2018-12-03 20:46:01 +0200

    Add change in Box2D allowing proper compilation on Aalto computers

--- src/libs/Box2D/Box2D/Common/b2Math.h
+++ src/libs/Box2D/Box2D/Common/b2Math.h
@@ -21,11 +21,12 @@
 
 #include "Box2D/Common/b2Settings.h"
 #include <math.h>
+#include <ctgmath>
 
 /// This function is used to ensure that a floating point number is not a NaN or infinity.
 inline bool b2IsValid(float32 x)
 {
-	return isfinite(x);
+	return std::isfinite(x);
 }
 
 #define	b2Sqrt(x)	sqrtf(x)
